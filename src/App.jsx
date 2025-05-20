09728c21815c9accb26f60170df4c6aef33e540a
Pedro Roberto <maltarollipedro@gmail.com>
2025-05-20 13:49:01 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl bg-sky-950 font-bold underline text-center">
+        Hello world!
+      </h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>Ola mundo 2</h1>;
+}
+
+export default Header;
